9a84c20fb569f5896222f52eaf8235c549af63a6
Windpicker-owo <3431391539@qq.com>
2026-05-06 16:08:40 +0800

    feat: 更新版本号至 1.1.0-rc，优化表情包处理逻辑，添加相关测试

--- src/core/config/core_config.py
+++ src/core/config/core_config.py
@@ -7,7 +7,7 @@ from typing import Literal
 
 from src.kernel.config import ConfigBase, SectionBase, config_section, Field
 
-CORE_VERSION = "1.1.0-beta.1"
+CORE_VERSION = "1.1.0-rc"
 
 class CoreConfig(ConfigBase):
     """Core 层配置类
